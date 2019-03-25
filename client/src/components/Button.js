35a51e032eeeb87464d297151f710de058922702
rebecca112233 <rebecca.shi@nyu.edu>
2019-03-25 19:40:33 -0400

    added initial travis.yml

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Button extends React.Component {
+export default class GenButton extends React.Component {
     constructor(props) {
         super(props);
         this.state = { buttonText: 'Click me' };
