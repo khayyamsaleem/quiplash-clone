b68fdca3c36baf392ad5fae4d395726479b8c9dd
rebecca112233 <rebecca.shi@nyu.edu>
2019-03-26 21:18:28 -0400

    updated front page style

--- client/src/App.js
+++ client/src/App.js
@@ -7,29 +7,31 @@ import { Nav, Navbar, Button, Container, Row, Col } from 'react-bootstrap';
 
 const App = () => (
   <>
-    <Navbar fixed="top" bg="info">
-      <Navbar.Brand href="#home">
-        <img alt="quip logo" src={logo} width="85" height="44.25" className="d-inline-block align-top"/>
-      </Navbar.Brand>
-      <Nav.Link href="#">About</Nav.Link>
-    </Navbar>
+    <div class="App">
+      <Navbar fixed="top" bg="info">
+        <Navbar.Brand href="#home">
+          <img alt="quip logo" src={logo} width="85" height="44.25" className="d-inline-block align-top"/>
+        </Navbar.Brand>
+        <Nav.Link href="#" style={{color:'white'}}>About</Nav.Link>
+      </Navbar>
 
-    <div className="contentContainer title" id="first">
-      <h1>Quip Thinking</h1>
-      <h4>the online game of witty lines</h4> 
-    </div>
+      <div className="contentContainer heading" id="first">
+        <h1 id="title">Quip Thinking</h1>
+        <h4 id="byline">the online game of witty lines</h4> 
+      </div>
 
-    <div className="contentContainer" id="second">
-      <h2>Play now!</h2>
-      <Container>
-        <Row>
-          <Col><Button variant="primary">Create a Room</Button></Col>
-          <Col><Button variant="success">Join a Room</Button></Col>
-          <Col><Button variant="danger">Add a Prompt</Button></Col>
-        </Row>
-      </Container>
-      <hr />
-      <Scoreboard />
+      <div className="contentContainer" id="second">
+        <h2>Play now!</h2>
+        <Container>
+          <Row>
+            <Col><Button variant="primary">Create a Room</Button></Col>
+            <Col><Button variant="success">Join a Room</Button></Col>
+            <Col><Button variant="danger">Add a Prompt</Button></Col>
+          </Row>
+        </Container>
+        <hr />
+        <Scoreboard />
+      </div>
     </div>
   </>
 )
