1b427e1eb964863fe9b7d0c2ed2d7f1195b75f01
rebecca112233 <rebecca.shi@nyu.edu>
2019-03-31 23:46:20 -0400

    updated nav bar with buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.png';
-import GenButton from './components/Button';
-import Scoreboard from './components/Scoreboard';
+// import GenButton from './components/Button';
+// import Scoreboard from './components/Scoreboard';
 import { Nav, Navbar, Button, Container, Row, Col } from 'react-bootstrap';
 
 const App = () => (
   <>
     <div class="App">
-      <Navbar fixed="top" bg="info">
+      <Navbar fixed="top" bg="info" expand="lg">
         <Navbar.Brand href="#home">
           <img alt="quip logo" src={logo} width="85" height="44.25" className="d-inline-block align-top"/>
         </Navbar.Brand>
-        <Nav.Link href="#" style={{color:'white'}}>About</Nav.Link>
+        {/* <Nav.Link href="#" style={{color:'white'}}>About</Nav.Link> */}
+        <Container className="justify-content-end">
+          <Row>
+            <Nav.Link href="#"><Col><Button variant="primary">Create a Room</Button></Col></Nav.Link>
+            <Nav.Link href="#"><Col><Button variant="success">Join a Room</Button></Col></Nav.Link>
+            <Nav.Link href="#"><Col><Button variant="danger">Add a Prompt</Button></Col></Nav.Link>
+          </Row>
+        </Container>
       </Navbar>
 
       <div className="contentContainer heading" id="first">
         <h1 id="title">Quip Thinking</h1>
         <h4 id="byline">the online game of witty lines</h4> 
       </div>
-
-      <div className="contentContainer" id="second">
+      
+      {/* <div className="contentContainer" id="second">
         <h2>Play now!</h2>
         <Container>
           <Row>
@@ -29,7 +36,7 @@ const App = () => (
             <Col><Button variant="danger">Add a Prompt</Button></Col>
           </Row>
         </Container>
-      </div>
+      </div> */}
 
       {/* <div className="contentContainer" id="third">
         <hr />
