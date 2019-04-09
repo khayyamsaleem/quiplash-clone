f6ca15a3ae83a20a052b88bf398c7d2af7db30b1
rebecca112233 <rebecca.shi@nyu.edu>
2019-04-09 13:31:43 -0400

    added initial instruction page

--- client/src/pages/instructions/instructions.js
+++ client/src/pages/instructions/instructions.js
@@ -6,11 +6,15 @@ export default class Instructions extends React.Component{
         return(
             <div id="instructions">
                 <h2>How Do I Play?</h2>
-                <div className="box">
-                
-                </div>
-                <div className="box">
-
+                <div id="boxes">
+                    <div className="box" id="box1">
+                        <h4>Starting a Game</h4>
+                        <h5>Creating a Room</h5>
+                        <h5>Joining a Room</h5>
+                    </div>
+                    <div className="box" id="box2">
+                        <h4>Playing the Game</h4>
+                    </div>
                 </div>
             </div>
         )
