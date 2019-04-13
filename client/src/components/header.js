70cb2313fe01d2590a1918da76359fe217ca111e
rebecca112233 <rebecca.shi@nyu.edu>
2019-04-12 22:09:59 -0400

    updated homepage and navbar colors

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -14,18 +14,10 @@ export default class Header extends React.Component{
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" style={{ float: 'right'}}>
             <Nav>
-              {/* <Container className="justify-content-end" style={{ float: 'right'}}> */}
-                {/* <Row> */}
-                  {/* <Nav.Link href="/CreateRoom"><Col><Button variant="primary">Create a Room</Button></Col></Nav.Link>
-                  <Nav.Link href="/JoinRoom"><Col><Button variant="success">Join a Room</Button></Col></Nav.Link>
-                  <Nav.Link href="#"><Col><Button variant="danger">Add a Prompt</Button></Col></Nav.Link> */}
-
               <Nav.Link href="/CreateRoom" style={{color: 'white'}}><Button style={{backgroundColor: '#66BB6A'}}>Create a Private Room</Button></Nav.Link>
               <Nav.Link href="/CreateRoom" style={{color: 'white'}}><Button style={{backgroundColor: '#26A69A'}}>Create a Public Room</Button></Nav.Link>
               <Nav.Link href="/JoinRoom" style={{color: 'white'}}><Button style={{backgroundColor: '#BA68C8'}}>Join a Room</Button></Nav.Link>
               <Nav.Link href="#" style={{color: 'white'}}><Button style={{backgroundColor: '#EF5350'}}>Add a Prompt</Button></Nav.Link>
-                {/* </Row> */}
-              {/* </Container> */}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
