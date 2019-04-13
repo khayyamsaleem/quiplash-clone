ce2089d464582911c6211668107bdeaded9ab436
Larry Liu <ll3279@nyu.edu>
2019-04-13 01:09:39 -0400

    fix linting warnings

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.png';
+// import logo from './logo.png';
 import Home from './pages/home/home';
 // import GenButton from './components/Button';
 // import Scoreboard from './components/Scoreboard';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/header";
 import JoinRoom from "./pages/join/JoinRoom";
 import CreateRoom from "./pages/create/CreateRoom";
--- client/src/components/Scoreboard.js
+++ client/src/components/Scoreboard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default class Scoreboard extends React.Component {
-    constructor(props) {
-        super(props);
-        const playerCount = 5;
-        // this.state = { };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     const playerCount = 5;
+    //     // this.state = { };
+    // }
 
     render() {
       const names = ["Disha", "Jody", "Larry", "Rebecca", "Richard"];
