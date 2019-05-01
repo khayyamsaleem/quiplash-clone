639e3276bd75e284b0a090454d61f0ccd094a3e7
rebecca112233 <rebecca.shi@nyu.edu>
2019-05-01 13:58:44 -0400

    updated add page to modal

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import Home from './pages/home/home';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/header";
 import JoinRoom from "./pages/join/JoinRoom";
-import AddPrompt from "./pages/add/addPrompt";
 import Error from "./pages/error/Error";
 import Game from "./pages/games/Game";
 
@@ -19,7 +18,6 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path = "/JoinRoom" component={JoinRoom}/>
-          <Route path = "/AddPrompt" component={AddPrompt} />
           <Route path = "/Game" component={Game} />
           <Route component={Error}/>
         </Switch>
