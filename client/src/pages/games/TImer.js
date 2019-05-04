b01ed42442cdd7c059f7c9fa3cc5db863a21a174
Disha Gupta <dishagupta275@gmail.com>
2019-05-04 17:40:52 -0400

    frontend timer

--- client/src/pages/games/TImer.js
+++ client/src/pages/games/TImer.js
@@ -1,66 +1,46 @@
 import React from "react";
 
-// 
-
 const timer = () => {
 	return(
-		//
+			<div className="timer">
+
+				<p> Time Left: </p>			
+
+				<Row>
+
+				<Col>
+
+					<button onclick="setTimeout(myFunction, 90000)">timer</button>
+
+					<script>
+						function myFunction() {
+							alert("time over");
+						}
+					</script>
+
+				</Col>
+
+				</Row>
+
+			</div>
 	)
 }
 
-
-  return(
-<>
-  <div className="create">
-  <Header/>
-  <Jumbotron>
-    <h1>Round #</h1>
-    <p>
-      SAMPLE PROMPT HERE
-    </p>
-  </Jumbotron>
-  <Row>
-    <Col>
-      {/* we need to figure out how to hold button animation until all
-        user votes go through or time runs out */}
-      <Nav.Link href="#"><Button variant="light">QUIP 1 HERE </Button></Nav.Link>
-    </Col>
-    <Col>
-      <Nav.Link href="#"><Button variant="dark">QUIP 2 HERE</Button></Nav.Link>
-    </Col>
-  </Row>
-  </div>
-</>
-
-  )
-
-}
-
-// 
-
-<button onclick="setTimeout(myFunction, 90000)">timer</button>
-
-<script>
-	function myFunction() {
-		alert("time over");
-	}
-</script>
-
 // notes: https://robdodson.me/building-a-countdown-timer-with-socket-dot-io/
 
-var countdown = 90000;
+// var countdown = 90000;
 
-timer(function() {
-	countdown--;
+// timer(function() {
+// 	countdown--;
 
-	io.sockets.emit("timer", {countdown: countdown});
+// 	io.sockets.emit("timer", {countdown: countdown});
 
-}, 5000);
+// }, 5000);
 
-io.sockets.on("on", function(socket) {
-	socket.on("reset", function(data) {
-		countdown = 90000;
+// io.sockets.on("on", function(socket) {
+// 	socket.on("reset", function(data) {
+// 		countdown = 90000;
 
-		io.sockets.emit("timer", {countdown: countdown});
-	});
-});
+// 		io.sockets.emit("timer", {countdown: countdown});
+// 	});
+// });
