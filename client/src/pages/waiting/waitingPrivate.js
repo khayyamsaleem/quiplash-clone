24ba07e007c15bde15ab412a1a6a3794fbd68434
rebecca112233 <rebecca.shi@nyu.edu>
2019-05-04 19:13:32 -0400

    updated waiting page

--- client/src/pages/waiting/waitingPrivate.js
+++ client/src/pages/waiting/waitingPrivate.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './waiting.css';
+import {Button} from 'react-bootstrap';
 
 export default class WaitingPrivate extends React.Component{
   render(){
@@ -8,11 +9,16 @@ export default class WaitingPrivate extends React.Component{
         <div id="waitingPrivate">
           <div id="heading">
             <h1>{this.props.roomName}</h1>
-            <h2 id="code">Room Code: {this.props.roomId}</h2>
+            <h2 id="code">Room Code: {this.props.roomCode}</h2>
             <h2 id="time">Timer: </h2>
           </div>
           <div id="players">
+            <h3>Player 1: {this.props.playerName}</h3>
+            <h3>Player 2: </h3>
+            <h3>Player 3: </h3>
+            <h3>Player 4: </h3>
           </div>
+          <Button variant="primary" type="submit">Start the Game!</Button>
         </div>
       </>
     )
