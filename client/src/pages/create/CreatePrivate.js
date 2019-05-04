25291040690a8e5a7ad53a3ac26064f52f305690
rebecca112233 <rebecca.shi@nyu.edu>
2019-05-04 19:13:04 -0400

    made create private page

--- client/src/pages/create/CreatePrivate.js
+++ client/src/pages/create/CreatePrivate.js
@@ -1,66 +1,48 @@
 import React from 'react';
 import './create.css';
-import { Form, Row, Col, Button } from 'react-bootstrap';
-import Header from "../../components/header";
 
-const CreatePrivate = () => {
-  return(
-<>
-  <Header>
-  <div className="create">
-    <Form>
-      <h1> SAMPLE PRIVATE ROOM CODE</h1>
-      <br />
-      <Row>
-        <Col>
-          <Form.Control placeholder="You Are Here" />
-        </Col>
-        <Col>
-          <Form.Control placeholder="Player 5 Slot Still Open" />
-        </Col>
-      </Row>
+export default class CreatePrivate extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      roomCode: 0,
+      roomName: "",
+      playerName: "",
+      readyToSubmit: false
+    };
 
-      <br />
-      <Row>
-        <Col>
-          <Form.Control placeholder="Player 2 Slot Still Open" />
-        </Col>
-        <Col>
-          <Form.Control placeholder="Player 6 Slot Still Open" />
-        </Col>
-      </Row>
+    let socket=io.connect('http://localhost:8080');
+    socket.on('connection',() => {
+      socket.emit('create-private-room', (message) => {
+        this.setState({roomCode: message});
+      })
+    })
 
-      <br />
-      <Row>
-        <Col>
-          <Form.Control placeholder="Player 3 Slot Still Open" />
-        </Col>
-        <Col>
-          <Form.Control placeholder="Player 7 Slot Still Open" />
-        </Col>
-      </Row>
+    socket.disconnect();
+    
+  }
+  //STORES ROOM NAME AND PLAYER NAME IN STATE
+  handleRoomChange(e){
+    this.setState({roomName: e.target.value});
+  }
+  handleNameChange(e){
+    this.setState({playerName: e.target.value});
+  }
+  submitForm () {
+    this.setState({readyToSubmit: true});
+  }
 
-      <br />
-      <Row>
-        <Col>
-          <Form.Control placeholder="Player 4 Slot Still Open" />
-        </Col>
-        <Col>
-          <Form.Control placeholder="Player 8 Slot Still Open" />
-        </Col>
-      </Row>
-
-      <br/>
-      <Button variant="primary" type="submit">
-        Start the Game!
-      </Button>
-    </Form>
-  </div>
-  </Header>
-</>
-
-  )
-
+  render(){
+    return(
+      this.state.readyToSubmit 
+      ? <WaitingPrivate roomCode={this.state.roomCode} socket={this.socket} roomName = {this.state.roomName} playerName = {this.state.playerName}/>
+      : <Form style={{width: '100%'}}>
+          <Form.Label>Room Name</Form.Label>
+          <Form.Control placeholder="Enter a room name" onChange={this.handleRoomChange.bind(this)}/>
+          <Form.Label>Player Name</Form.Label>
+          <Form.Control placeholder="Enter your player name" onChange={this.handleNameChange.bind(this)}/>
+        </Form>
+        <Button onClick={this.submitForm.bind(this)}>Submit</Button>
+    )
+  }
 }
-
-export default CreatePrivate;
